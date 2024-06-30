f712547b2cb1f9121bce68c8d4f2c6c7fb9b9d3c
Palapajr <qumfaanzirr@gmail.com>
2024-06-30 13:30:10 +0700

    t

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,16 +10,16 @@ use Illuminate\Support\Facades\Validator;
 class HomeController extends Controller
 {
 
-    public function dashboard()
-    {
-        return view('dashboard');
-    }
-
     public function index()
     {
         $data = User::get();
 
         return view('index', compact('data'));
+    }
+
+    public function dashboard()
+    {
+        return view('dashboard');
     }
 
     public function create()
